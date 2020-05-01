3dd53db6047d2f7ae75224460a1a73cb7717210e
jimjoyce <j.joyce92@gmail.com>
2020-05-01 07:01:25 -0500

    Adds more header paths

--- StatefulTextField/StatefulTextField.h
+++ StatefulTextField/StatefulTextField.h
@@ -12,6 +12,8 @@
 FOUNDATION_EXPORT double StatefulTextFieldVersionNumber;
 
 //! Project version string for StatefulTextField.
-FOUNDATION_EXPORT const unsigned char StatefulTextFieldVersionString[1.0];
+FOUNDATION_EXPORT const unsigned char StatefulTextFieldVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <StatefulTextField/PublicHeader.h>
+
+
